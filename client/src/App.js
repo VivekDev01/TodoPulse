234036d6ad99aaf084027ef503e7e518ce79ee5d
VivekDevShah <vivek65.ugcs20@iiitranchi.ac.in>
2023-10-05 03:39:48 +0530

    UI for home starts

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route
             path='/'
-            element={<h1>Home page</h1>}
+            element={<Home />}
           />
 
           <Route
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/home.css"
+
+const Home = () => {
+  return (
+    <div className='outermost'>
+        
+    </div>
+  )
+}
+
+export default Home
